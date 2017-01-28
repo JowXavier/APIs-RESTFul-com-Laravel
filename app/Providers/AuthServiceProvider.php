dbc1a72b22c7101a0ab195509ba7b2219a61fe30
Jonathan Xavier <jonathanxribeiro@gmail.com>
2017-01-28 12:08:58 -0200

    alterando provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Carbon\Carbon;
 
@@ -28,6 +29,10 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Route::group([ 'middleware' => 'cors'], function() {
+            Passport::routes();
+        });
+  
         Passport::tokensExpireIn(Carbon::now()->addHours(1));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
     }
